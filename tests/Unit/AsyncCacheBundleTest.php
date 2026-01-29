2bd4351f5489dc4d4468155f8c88e91d702b633a
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-01-29 16:48:01 +0200

    test: Add unit test for AsyncCacheBundle build method

---
+++ tests/Unit/AsyncCacheBundleTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Fyennyi\AsyncCache\Bridge\Symfony\Tests\Unit;
+
+use Fyennyi\AsyncCache\Bridge\Symfony\AsyncCacheBundle;
+use Fyennyi\AsyncCache\Bridge\Symfony\DependencyInjection\Compiler\MiddlewarePass;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class AsyncCacheBundleTest extends TestCase
+{
+    public function testBuildRegistersMiddlewarePass(): void
+    {
+        $bundle = new AsyncCacheBundle();
+        $container = $this->createMock(ContainerBuilder::class);
+
+        $container->expects($this->once())
+            ->method('addCompilerPass')
+            ->with($this->isInstanceOf(MiddlewarePass::class));
+
+        $bundle->build($container);
+    }
+}
