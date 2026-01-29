360e4b18d20832644c9abb88acb0bbf0925673c2
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-01-29 15:54:50 +0200

    refactor: Update AsyncCacheExtension to use named arguments for service definition

--- src/DependencyInjection/AsyncCacheExtension.php
+++ src/DependencyInjection/AsyncCacheExtension.php
@@ -8,22 +8,29 @@ use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Reference;
 
+/**
+ * Dependency injection extension for Symfony.
+ */
 class AsyncCacheExtension extends Extension
 {
+    /**
+     * Loads the configuration and registers the AsyncCacheManager service.
+     *
+     * @param array            $configs   The configuration array
+     * @param ContainerBuilder $container The container builder
+     */
     public function load(array $configs, ContainerBuilder $container) : void
     {
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $definition = new Definition(AsyncCacheManager::class, [
-            new Reference('cache.app'),
-            null,
-            new Reference('logger'),
-            new Reference('lock.factory'),
-            [],
-            new Reference('event_dispatcher'),
-            null,
-            null,
+        $definition = new Definition(AsyncCacheManager::class);
+
+        $definition->setArguments([
+            '$cache_adapter' => new Reference('cache.app'),
+            '$logger'        => new Reference('logger'),
+            '$lock_factory'  => new Reference('lock.factory'),
+            '$dispatcher'    => new Reference('event_dispatcher'),
         ]);
 
         $definition->setPublic(true);
