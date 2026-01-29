a9fcee860cbeb4dab90fb6ca88e69eec5c3d31a5
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-01-29 16:10:33 +0200

    feat: Register MiddlewarePass in AsyncCacheBundle build method

--- src/AsyncCacheBundle.php
+++ src/AsyncCacheBundle.php
@@ -2,9 +2,19 @@
 
 namespace Fyennyi\AsyncCache\Bridge\Symfony;
 
+use Fyennyi\AsyncCache\Bridge\Symfony\DependencyInjection\Compiler\MiddlewarePass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 /**
  * Symfony Bundle for AsyncCache integration.
  */
-class AsyncCacheBundle extends Bundle {}
+class AsyncCacheBundle extends Bundle
+{
+    public function build(ContainerBuilder $container) : void
+    {
+        parent::build($container);
+
+        $container->addCompilerPass(new MiddlewarePass());
+    }
+}
