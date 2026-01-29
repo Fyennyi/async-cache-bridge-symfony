4335e49a7f007d00e7bd78cd4697806890bbc36f
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-01-29 09:52:36 +0200

    feat: Add AsyncCacheBundle for Symfony bridge

---
+++ src/Bridge/Symfony/AsyncCacheBundle.php
@@ -0,0 +1,13 @@
+<?php
+namespace Fyennyi\AsyncCache\Bridge\Symfony;
+
+class AsyncCacheBundle
+{
+    public function getContainerExtension()
+    {
+        if (class_exists('Fyennyi\\AsyncCache\\Bridge\\Symfony\\DependencyInjection\\AsyncCacheExtension')) {
+            return new \Fyennyi\AsyncCache\Bridge\Symfony\DependencyInjection\AsyncCacheExtension();
+        }
+        return null;
+    }
+}
