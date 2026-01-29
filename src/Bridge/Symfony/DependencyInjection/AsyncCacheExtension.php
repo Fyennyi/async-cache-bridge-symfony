ac20a1576d37576e62c73848ff22cf09a86dea30
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-01-29 09:53:56 +0200

    feat: Add AsyncCacheExtension for Symfony bridge

---
+++ src/Bridge/Symfony/DependencyInjection/AsyncCacheExtension.php
@@ -0,0 +1,32 @@
+<?php
+namespace Fyennyi\AsyncCache\Bridge\Symfony\DependencyInjection;
+
+use Fyennyi\AsyncCache\AsyncCacheManager;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\DependencyInjection\Reference;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Fyennyi\AsyncCache\Bridge\Symfony\DependencyInjection\Configuration;
+
+class AsyncCacheExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container): void
+    {
+        $configuration = new Configuration();
+        $config = $this->processConfiguration($configuration, $configs);
+
+        $definition = new Definition(AsyncCacheManager::class, [
+            new Reference('cache.app'),
+            null,
+            new Reference('logger'),
+            new Reference('lock.factory'),
+            [],
+            new Reference('event_dispatcher'),
+            null,
+            null,
+        ]);
+
+        $definition->setPublic(true);
+        $container->setDefinition(AsyncCacheManager::class, $definition);
+    }
+}
