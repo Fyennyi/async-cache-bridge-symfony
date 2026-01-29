622cb3fc1cccf45c2b9b354549b2ca00620b91e4
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-01-29 09:53:12 +0200

    feat: Add Configuration for Symfony bridge DI

---
+++ src/Bridge/Symfony/DependencyInjection/Configuration.php
@@ -0,0 +1,21 @@
+<?php
+namespace Fyennyi\AsyncCache\Bridge\Symfony\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+class Configuration implements ConfigurationInterface
+{
+    public function getConfigTreeBuilder(): TreeBuilder
+    {
+        $treeBuilder = new TreeBuilder('async_cache');
+        $rootNode = method_exists($treeBuilder, 'getRootNode') ? $treeBuilder->getRootNode() : $treeBuilder->root('async_cache');
+
+        $rootNode
+            ->children()
+                ->scalarNode('default_strategy')->defaultValue('strict')->end()
+            ->end();
+
+        return $treeBuilder;
+    }
+}
